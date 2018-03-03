29fb421ed2e483e18af245e3089501b76803ff11
Nick Wilson <logikfx@gmail.com>
2018-03-02 23:15:21 -0500

    program ignores faulty user input

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
@@ -23,8 +23,10 @@ fn main() {
         io::stdin().read_line(&mut guess).expect("Failed to read line");
 
         // convert/parse number from string
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         // how to sub data into strings
         println!("You guessed: {}", guess);
