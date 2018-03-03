5fba1210bbb2ab33ff1b5cb9c0cafacc555266ef
Nick Wilson <logikfx@gmail.com>
2018-03-02 23:00:38 -0500

    added random number to game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 // bring in io library from standard library
 
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess.");
 
